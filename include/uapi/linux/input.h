5475b147355ab4afafde1fbe23f4899dbce7f755
Scott Mertz <scott@cyngn.com>
2015-01-19 11:13:15 -0800

    input: touchscreen: gsl1688e: Import GSL1688E touchscreen driver
    * Package version: cm-12.1-amss-1.0.2:
      f8056a2ce7ec0068ae373c91b52cd88f6a1c9c16
    
    2016-03-17      Keith Mok       input: touchscreen: gsl1688e: Send KEY_WAKEUP for double-tap event
    2016-11-16      Pranav Vashi    input: gsl1688e: Remove garbage comment
    2016-12-07      Michael Bestas  input: gsl1688e: Send custom keycodes for gestures and add sysfs nodes
    2016-12-09      Michael Bestas  input: gsl1688e: Doze after reporting a gesture
    2017-02-16      Louis Popi      input: touchscreen: gsl_ts: Handle more framebuffer hints
    
    Change-Id: I4f5fc87a6c13a6c335ff5c138fe6f987c9c0aaf1

--- include/uapi/linux/input.h
+++ include/uapi/linux/input.h
@@ -476,6 +476,20 @@ struct input_keymap_entry {
 
 #define KEY_MICMUTE		248	/* Mute / unmute the microphone */
 
+/* Gesture keycodes */
+#define KEY_GESTURE_SLIDE_DOWN		249
+#define KEY_GESTURE_SLIDE_LEFT		250
+#define KEY_GESTURE_SLIDE_RIGHT		251
+#define KEY_GESTURE_SLIDE_C		252
+#define KEY_GESTURE_SLIDE_O		253
+#define KEY_GESTURE_SLIDE_UP		254
+#define KEY_GESTURE_SLIDE_E		255
+#define KEY_GESTURE_SLIDE_M		256
+#define KEY_GESTURE_SLIDE_W		257
+#define KEY_GESTURE_SLIDE_Z		258
+#define KEY_GESTURE_SLIDE_V		259
+#define KEY_GESTURE_SLIDE_S		260
+
 /* Code 255 is reserved for special needs of AT keyboard driver */
 
 #define BTN_MISC		0x100
