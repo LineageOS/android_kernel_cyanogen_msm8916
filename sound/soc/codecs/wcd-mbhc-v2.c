d3308bd750a91ea3fe486ef5563820c93e74e1ee
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-05-30 23:10:20 +0800

    ASoC: wcd: Override headset button thresholds for CP8675
    tomato needs these for its buttons to function properly.
    
    Change-Id: Ic0a3f561565cbbf76543c20805e52931f3a8061a

--- sound/soc/codecs/wcd-mbhc-v2.c
+++ sound/soc/codecs/wcd-mbhc-v2.c
@@ -28,6 +28,7 @@
 #include <glink_private.h>
 #include <sound/soc.h>
 #include <sound/jack.h>
+#include "msm8x16_wcd_registers.h"
 #include "wcd-mbhc-v2.h"
 #include "wcdcal-hwdep.h"
 
@@ -140,6 +141,14 @@ static void wcd_program_btn_threshold(const struct wcd_mbhc *mbhc, bool micbias)
 
 	mbhc->mbhc_cb->set_btn_thr(codec, btn_low, btn_high, btn_det->num_btn,
 				   micbias);
+
+#ifdef CONFIG_MACH_CP8675
+	snd_soc_update_bits(codec, MSM8X16_WCD_A_ANALOG_MBHC_BTN0_ZDETL_CTL, 0xFC, 0x20);
+	snd_soc_update_bits(codec, MSM8X16_WCD_A_ANALOG_MBHC_BTN1_ZDETM_CTL, 0xFC, 0x40);
+	snd_soc_update_bits(codec, MSM8X16_WCD_A_ANALOG_MBHC_BTN2_ZDETH_CTL, 0xFC, 0x68);
+	snd_soc_update_bits(codec, MSM8X16_WCD_A_ANALOG_MBHC_BTN3_CTL, 0xFC, 0x78);
+	snd_soc_update_bits(codec, MSM8X16_WCD_A_ANALOG_MBHC_BTN4_CTL, 0xFC, 0x88);
+#endif
 }
 
 static void wcd_enable_curr_micbias(const struct wcd_mbhc *mbhc,
@@ -1858,6 +1867,10 @@ static int wcd_mbhc_initialise(struct wcd_mbhc *mbhc)
 	wcd_program_hs_vref(mbhc);
 
 	wcd_program_btn_threshold(mbhc, false);
+
+#ifdef CONFIG_MACH_CP8675
+	snd_soc_update_bits(codec, MSM8X16_WCD_A_ANALOG_MBHC_BTN3_CTL, 0x03, 0x03);
+#endif
 
 	INIT_WORK(&mbhc->correct_plug_swch, wcd_correct_swch_plug);
 
