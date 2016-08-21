f00aeaa0a7228563e549a6e2785dcd858dc0ce8b
Shoaib0597 <Shoaib0595@gmail.com>
2016-08-21 16:02:35 +0530

    Tune the Task-Scheduler for Power-Savings
    1. Enable HMP-Aware and Power-Aware Task-Scheduling.
    This will help in Saving More Power without any noticeable Performance-Hit. Further, this will also help the Scheduler to take Full Advantage of the big.LITTLE Architecture.
    
    2. Make the Task-Scheduler Utilize the LITTLE Cluster Aggressively.
    Alter the Task-Thresholds so that the Task-Scheduler uses LITTLE Cluster for most of the tasks in order to Save Power. Further, Android Background Processes have a Default Value of Min. Nice set to 10. Thus, Decrease the Min. Nice Value of Task-Scheduler to 5 in order to Prevent these Processes to be Scheduled on the big Cluster.
    
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1300,7 +1300,7 @@ unsigned int __read_mostly sysctl_sched_spill_load_pct = 100;
  * sched_small_task are considered as small tasks.
  */
 unsigned int __read_mostly sched_small_task;
-unsigned int __read_mostly sysctl_sched_small_task_pct = 10;
+unsigned int __read_mostly sysctl_sched_small_task_pct = 65;
 
 /*
  * Tasks with demand >= sched_heavy_task will have their
@@ -1321,7 +1321,7 @@ unsigned int __read_mostly sched_heavy_task;
  * capacity.
  */
 unsigned int __read_mostly sched_upmigrate;
-unsigned int __read_mostly sysctl_sched_upmigrate_pct = 80;
+unsigned int __read_mostly sysctl_sched_upmigrate_pct = 95;
 
 /*
  * Big tasks, once migrated, will need to drop their bandwidth
@@ -1329,14 +1329,14 @@ unsigned int __read_mostly sysctl_sched_upmigrate_pct = 80;
  * migrated.
  */
 unsigned int __read_mostly sched_downmigrate;
-unsigned int __read_mostly sysctl_sched_downmigrate_pct = 60;
+unsigned int __read_mostly sysctl_sched_downmigrate_pct = 80;
 
 /*
  * Tasks whose nice value is > sysctl_sched_upmigrate_min_nice are never
  * considered as "big" tasks.
  */
-static int __read_mostly sched_upmigrate_min_nice = 15;
-int __read_mostly sysctl_sched_upmigrate_min_nice = 15;
+static int __read_mostly sched_upmigrate_min_nice = 5;
+int __read_mostly sysctl_sched_upmigrate_min_nice = 5;
 
 /*
  * Tunable to govern scheduler wakeup placement CPU selection
