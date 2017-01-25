00e8333e777b0b1f76b773fa8c402e8e204e9b8f
Pranav Vashi <neobuddy89@gmail.com>
2017-01-26 01:31:33 +0530

    perf: Clean up spaces and fix mismerge
    Change-Id: Ib45e7822dd94b8ba0ff9511986e7d6d151964d7e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -3741,15 +3741,15 @@ static long _perf_ioctl(struct perf_event *event, unsigned int cmd,
 
 static long perf_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
-        struct perf_event *event = file->private_data;
-        struct perf_event_context *ctx;
-        long ret;
+	struct perf_event *event = file->private_data;
+	struct perf_event_context *ctx;
+	long ret;
 
-        ctx = perf_event_ctx_lock(event);
-        ret = _perf_ioctl(event, cmd, arg);
-        perf_event_ctx_unlock(event, ctx);
+	ctx = perf_event_ctx_lock(event);
+	ret = _perf_ioctl(event, cmd, arg);
+	perf_event_ctx_unlock(event, ctx);
 
-        return ret;
+	return ret;
 }
 
 #ifdef CONFIG_COMPAT
@@ -7187,7 +7187,6 @@ SYSCALL_DEFINE5(perf_event_open,
 		 */
 		mutex_lock_double(&gctx->mutex, &ctx->mutex);
 
-		mutex_lock(&gctx->mutex);
 		perf_remove_from_context(group_leader, false);
 
 		/*
