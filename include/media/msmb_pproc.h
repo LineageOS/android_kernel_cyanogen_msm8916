d821fd46a6273a42e23206dc178adac41e374b07
NeoDarkness <aldi.45ckr@gmail.com>
2017-01-30 22:52:59 +0000

    [DO NOT MERGE] msm8916: Add wt88047
    Change-Id: I013afe3659bbb0180683a12e01e0836c928c6f29

--- include/media/msmb_pproc.h
+++ include/media/msmb_pproc.h
@@ -1,3 +1,7 @@
+#ifdef CONFIG_WT88047_CAMERA
+#include <media/msmb_pproc_wt88047.h>
+#else
+
 #ifndef __MSMB_PPROC_H
 #define __MSMB_PPROC_H
 
@@ -427,3 +431,5 @@ struct msm_camera_v4l2_ioctl32_t {
 #endif
 
 #endif /* __MSMB_PPROC_H */
+
+#endif  /* CONFIG_WT88047_CAMERA */
