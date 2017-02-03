fa63062f511829702c5fbf8000dc083469e7a6bd
Steve Pfetsch <spfetsch@google.com>
2017-02-03 22:05:17 +0100

    drivers: video: Add bounds checking in fb_cmap_to_user
    Verify that unsigned int value will not become negative before cast to
    signed int.
    
    Bug: 31651010
    Change-Id: I548a200f678762042617f11100b6966a405a3920

--- drivers/video/fbcmap.c
+++ drivers/video/fbcmap.c
@@ -197,7 +197,7 @@ int fb_cmap_to_user(const struct fb_cmap *from, struct fb_cmap_user *to)
 	unsigned int tooff = 0, fromoff = 0;
 	size_t size;
 
-	if (!to || !from)
+	if (!to || !from || (int)(to->start) < 0)
 		return -EINVAL;
 
 	if (to->start > from->start)
