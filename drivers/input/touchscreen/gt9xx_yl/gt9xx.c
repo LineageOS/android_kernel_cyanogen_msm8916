1174d796c5420a9724e2d7babde7839eb5737de8
Louis Popi <theh2o64@gmail.com>
2017-02-16 06:16:24 +0100

    input: touchscreen: gt9xx_yl: Handle more framebuffer hints
    Resume touchscreen functions during doze mode to allow the user
    to interact with the screen.
    
    Change-Id: Ib0e20f6681be7fdddabea0a4e827569977f4e8ba

--- drivers/input/touchscreen/gt9xx_yl/gt9xx.c
+++ drivers/input/touchscreen/gt9xx_yl/gt9xx.c
@@ -1494,7 +1494,9 @@ static int fb_notifier_callback(struct notifier_block *self,
 	if (evdata && evdata->data && event == FB_EVENT_BLANK && ts_data &&
 			ts_data->client) {
 		blank = evdata->data;
-		if (*blank == FB_BLANK_UNBLANK)
+		if (*blank == FB_BLANK_UNBLANK
+			|| *blank == FB_BLANK_NORMAL
+			|| *blank == FB_BLANK_VSYNC_SUSPEND)
 			goodix_ts_resume(&ts_data->client->dev);
 		else if (*blank == FB_BLANK_POWERDOWN)
 			goodix_ts_suspend(&ts_data->client->dev);
