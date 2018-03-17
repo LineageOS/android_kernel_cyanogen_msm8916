8f3500a3614145e281233b4ee7bb28c98cb7ee9a
Michael Bestas <mkbestas@lineageos.org>
2018-03-17 21:23:03 +0200

    input: touchscreen: gsl1688e: Disable GSL_TIMER
    * It causes reboots at boot logo since cm-13.0
    
    Change-Id: I79cba8357b6e359f6117e6338fc5d010054c0dae

--- drivers/input/touchscreen/gsl_ts_driver.h
+++ drivers/input/touchscreen/gsl_ts_driver.h
@@ -28,7 +28,7 @@
 #endif
 
 //#define GSL_DEBUG		//调试信息开关，打开则输出调试信息
-#define GSL_TIMER		//定时器开关
+//#define GSL_TIMER		//定时器开关
 #define TPD_PROC_DEBUG	//proc文件系统接口红开关
 #define GSL9XX_VDDIO_1800  1
 //#define GSL_REPORT_POINT_SLOT
